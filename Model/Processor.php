6b45bda711585ff2808b96b6e561f347b9a1ea9a
rizalfauzie <rizal.fauzie@sirclo.com>
2021-07-18 09:16:01 +0700

    add logout topic

--- Model/Processor.php
+++ Model/Processor.php
@@ -10,21 +10,42 @@ class Processor
      * @param string $message
      * @return void
      */
-    public function execute(string $message)
+    public function execute($message)
     {
-        logger('RUN Processor::execute');
+        logger('== Processor execute');
+        sleep(10);
+        logger('== Processor sleep 10s end');
 
         $client = new \GuzzleHttp\Client([
-            'base_uri' => 'https://magento.requestcatcher.com'
+            'base_uri' => 'http://10.0.0.1:3000'
         ]);
 
         try {
+            if ($this->isJson($message)) {
+                $json = json_decode($message);
+            } else {
+                $json = [ 'comment' => $message ];
+            }
+
             $response = $client->post('/messages', [
-                'json' => [ 'text' => $message ]
+                'json' => $json
             ]);
+
             logger($response->getStatusCode() . ' : ' . $response->getReasonPhrase());
         } catch (\Exception $e) {
             logger($e->getCode() . ' : ' . $e->getMessage());
         }
+    }
+
+    /**
+     * Check if string is json encoded
+     *
+     * @param mixed $string
+     * @return bool
+     */
+    function isJson($string)
+    {
+        @json_decode($string);
+        return json_last_error() === JSON_ERROR_NONE;
     }
 }
